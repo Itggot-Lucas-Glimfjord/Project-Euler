0b21bee2bacdd68c60866a3d4b4964364815adbd
Itggot-Lucas-Glimfjord <lucas.glimfjord@itggot.se>
2015-10-06 17:31:18 +0200

    Problem 3 solved

--- lib/Problem 3.py
+++ lib/Problem 3.py
@@ -1,5 +1,10 @@
+def highest_factorial(num):
+    counter = 2
+    while counter < num ** 0.5:
+        while num % counter == 0:
+            num = num / counter
+        counter += 1
+    return num
 
 
-
-
-
+print highest_factorial(num=600851475143)
