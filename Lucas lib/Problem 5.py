c90b22ebc55a1fdc49a9c1c4590a33188462bf76
Itggot-Lucas-Glimfjord <lucas.glimfjord@itggot.se>
2015-10-12 15:35:25 +0200

    Problem 5 Progress

--- Lucas lib/Problem 5.py
+++ Lucas lib/Problem 5.py
@@ -2,21 +2,24 @@ prime_cache = [2, 3, 5, 7, 11, 13, 17, 19]
 
 
 def smallest_multiple(max):
-    curr = 1
+    curr = 2
     while curr<=max:
         factorial = {}
 
-        while curr > 1
+        if curr > 1:
             for prime in prime_cache:
-                if curr % prime == 0 and curr != prime:
+                value = curr
+                while value % prime == 0 and curr != prime:
+                    value / prime
                     if prime in factorial.keys():
                         factorial[prime] += 1
                     else:
                         factorial[prime] = 1
-            curr += 1
+        curr += 1
 
+        print factorial
 
-
+smallest_multiple(20)
 
 
 
