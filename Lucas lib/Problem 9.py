acfeab059c538b5ed6861ada6d4d1f60a7e07784
Itggot-Lucas-Glimfjord <lucas.glimfjord@itggot.se>
2015-10-13 08:50:04 +0200

    Problem 9 solved

--- Lucas lib/Problem 9.py
+++ Lucas lib/Problem 9.py
@@ -1,1 +1,11 @@
-__author__ = 'lucas.glimfjord'
+
+def pythagorean_triplet_sum(sum):
+    for c in range(sum):
+        for b in range(sum):
+            if c > b:
+                for a in range(sum-b):
+                    if b > a and a**2 + b**2 == c**2 and a + b +c == sum:
+                        return a * b * c
+
+print pythagorean_triplet_sum(1000)
+
